f4397b7f737cd52326a9b1fd68f132e8c98bfa13
naitikkumar <r.naitikkumar1992@gmail.com>
2021-03-27 12:18:07 +0530

    1st commit

---
+++ no_of_disks.ps1
@@ -0,0 +1,54 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern("[a-z][A-Z]")]
+    [ValidateLength(2,20)]
+    [String]$vmname,
+
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern("[a-z][A-Z]")]
+    [ValidateLength(2,20)]
+    [String]$resourcegroup
+
+)
+
+
+try
+{
+$vm = get-azvm -ResourceGroupName $resourcegroup -Name $vmname
+}
+catch
+{
+    Write-Error 'error in retiving details '+$_
+}
+
+$datas =  $vm.StorageProfile
+
+$datadisks = ($datas.DataDisks).name
+
+
+[int]$count = $datadisks.Count
+Write-Output 'count of disk '$count
+
+
+if($count -ne 0 -and $null -ne $count)
+{
+    foreach($disks in $datadisks)
+    {
+       try
+       {
+        $disks = Get-AzDisk -ResourceGroupName $vm.ResourceGroupName -DiskName $disks
+		Write-Output $disks
+        }
+        catch
+        {
+            write-error 'error while retiving disks '$_
+        }    
+    }
+   
+}
+else
+{
+
+Write-Output 'no data disk are attached to vm'
+exit
+}
